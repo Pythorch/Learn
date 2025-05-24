b546554ec1517a2112afb8d92df5426b88ab7140
Pythorch <pythorch1@gmail.com>
2025-05-24 11:56:11 -0500

    Commit stash@{0}

--- learnpython.test.py
+++ learnpython.test.py
@@ -16,6 +16,10 @@
 
 # File edited under branch 'master'.
 
+<<<<<<< Updated upstream
 
 
 # File edited under branch 'master' after saved working directory with 'git stash'.
+=======
+# File edited under branch 'master' (branch 'stage' created in the past).
+>>>>>>> Stashed changes
