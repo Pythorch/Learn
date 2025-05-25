1e065edbf33f68ee0936877258174bd83945523b
Pythorch <pythorch1@gmail.com>
2025-05-25 15:11:01 -0500

    Add learnpython.test2.py

---
+++ learnpython.test2.py
@@ -0,0 +1,32 @@
+# Single line comments start with a number symbol.
+
+""" Multiline strings can be written
+    using three "s, and are often used
+    as documentation.
+"""
+
+# File edited on branch 'SDN'. 
+
+
+# File edited under branch 'auth'.
+
+
+#File edited under branch 'dev'.
+
+
+# File edited under branch 'master'.
+
+# File edited under branch 'master' after saved working directory with 'git stash'.
+
+# File edited under branch 'master' (branch 'stage' created in the past).
+
+############################################################
+
+File edited on https://github.com/Pythorch/Learn/blob/master/learnpython.test.py
+
+
+File edited on local machine.
+
+############################################################
+
+# Line edited with Pythorch user.
